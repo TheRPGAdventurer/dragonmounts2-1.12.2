b8312e67e2d86be9409e96742a8da4523bb2be85
TheRPGAdventurer <kingjamesxj9@gmail.com>
2019-04-08 14:03:32 +0800

    asddas

--- src/main/java/com/TheRPGAdventurer/ROTD/server/initialization/ModBlocks.java
+++ src/main/java/com/TheRPGAdventurer/ROTD/server/initialization/ModBlocks.java
@@ -1,17 +1,17 @@
 package com.TheRPGAdventurer.ROTD.server.initialization;
 
 import com.TheRPGAdventurer.ROTD.server.blocks.BlockDragonNest;
-import com.TheRPGAdventurer.ROTD.server.blocks.BlockDragonShulker;
 import net.minecraft.block.Block;
 
 public class ModBlocks {
 	
 	public static Block pileofsticks = new BlockDragonNest("pileofsticks");
-	public static Block dragon_shulker_box = new BlockDragonShulker();
+//	public static Block dragon_shulker_box = new BlockDragonShulker();
 	//public static Block pileofstickssnow = new BlockDragonNest("pileofstickssnow");
 	
 	public static final Block[] BLOCKS = {
-		pileofsticks, dragon_shulker_box//pileofstickssnow,
+		pileofsticks,
+//			dragon_shulker_box//pileofstickssnow,
 
 	};
 	
--- src/main/java/com/TheRPGAdventurer/ROTD/server/items/specialset/ItemEnderDragonArmour.java
+++ src/main/java/com/TheRPGAdventurer/ROTD/server/items/specialset/ItemEnderDragonArmour.java
@@ -20,9 +20,9 @@ import java.util.List;
 import java.util.Random;
 
 public class ItemEnderDragonArmour extends ItemArmor {
-	
+
 	private final PotionEffect potionEffect;
-    public EnumItemBreedTypes type;
+	public EnumItemBreedTypes type;
 
 	public ItemEnderDragonArmour(ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn, String unlocalizedName, PotionEffect potionEffect2, EnumItemBreedTypes type) {
 		super(materialIn, renderIndexIn, equipmentSlotIn);
@@ -32,29 +32,29 @@ public class ItemEnderDragonArmour extends ItemArmor {
 		this.setCreativeTab(DragonMounts.TAB);
 		this.type = type;
 	}
-	
+
 	@Override
 	public void onArmorTick(final World world, final EntityPlayer player, final ItemStack itemStack) {
 		if (!player.isPotionActive(potionEffect.getPotion())) { // If the Potion isn't currently active,
 			player.addPotionEffect(new PotionEffect(potionEffect)); // Apply a copy of the PotionEffect to the player
-		}	
-		
+		}
+
 		spawnParticles(player);
 	}
-	
+
 	public void spawnParticles(EntityPlayer player) {
-        // spawn generic particles
+		// spawn generic particles
 		Random rand = new Random();
-        double px = player.posX + (rand.nextDouble()) - 0.3; //  - 0.3
-        double py = player.posY + (rand.nextDouble())- 0.3;
-        double pz = player.posZ + (rand.nextDouble()) - 0.3;
-        double ox = (rand.nextDouble()) * 2- 0.3;
-        double oy = (rand.nextDouble()) * 2- 0.3;
-        double oz = (rand.nextDouble()) * 2- 0.3;
-        player.world.spawnParticle(EnumParticleTypes.PORTAL, px, py, pz, ox, oy, oz); 
-        
+		double px = player.posX + (rand.nextDouble() - 0.3);
+		double py = player.posY + (rand.nextDouble() - 0.3);
+		double pz = player.posZ + (rand.nextDouble() - 0.3);
+		double ox = (rand.nextDouble() - 0.3) * 2;
+		double oy = (rand.nextDouble() - 0.3) * 2;
+		double oz = (rand.nextDouble() - 0.3) * 2;
+		player.world.spawnParticle(EnumParticleTypes.PORTAL, px, py, pz, ox, oy, oz);
+
 	}
-	
+
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {

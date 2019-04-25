26a43abcae31d00d98d3aa3f285e6c7d0d688bb4
TheRPGAdventurer <kingjamesxj9@gmail.com>
2019-04-26 01:25:25 +0800

    fixed the bows

--- src/main/java/com/TheRPGAdventurer/ROTD/server/initialization/ModItems.java
+++ src/main/java/com/TheRPGAdventurer/ROTD/server/initialization/ModItems.java
@@ -143,8 +143,8 @@ public class ModItems {
 //    public static final Item terra2_dragon_bow = new ItemDragonBow(EnumItemBreedTypes.TERRA2, ModItems.TerraDragonScales2);
     public static final Item moonlight_dragon_bow = new ItemDragonMoonlightBow(EnumItemBreedTypes.MOONLIGHT, ModItems.MoonlightDragonScales);
 //    public static final Item storm_dragon_bow2 = new ItemDragonMoonlightBow(EnumItemBreedTypes.ST, ModItems.MoonlightDragonScales);
-    public static final Item skelton_dragon_bow = new ItemDragonSkeletonBow(EnumItemBreedTypes.MOONLIGHT, Items.BONE);
-    public static final Item zombie_dragon_bow = new ItemDragonZombieBow(EnumItemBreedTypes.MOONLIGHT, Items.BONE);
+    public static final Item skelton_dragon_bow = new ItemDragonSkeletonBow(EnumItemBreedTypes.SKELETON, Items.BONE);
+    public static final Item zombie_dragon_bow = new ItemDragonZombieBow(EnumItemBreedTypes.ZOMBIE, Items.BONE);
 
     public static final Item item_dragon_shulker = new ItemDragonShulker(ModBlocks.dragonshulker);
     /**

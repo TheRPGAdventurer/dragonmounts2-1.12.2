64f21961a5325b83dc9e60f8667ca6f8c550282b
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-06-12 16:21:20 -0400

    d

--- src/main/java/com/TheRPGAdventurer/ROTD/event/IItemColorRegistration.java
+++ src/main/java/com/TheRPGAdventurer/ROTD/event/IItemColorRegistration.java
@@ -2,12 +2,8 @@ package com.TheRPGAdventurer.ROTD.event;
 
 import com.TheRPGAdventurer.ROTD.inits.ModItems;
 import com.TheRPGAdventurer.ROTD.items.IItemDragonOrbColour;
-import net.minecraftforge.client.FluidContainerColorer;
+
 import net.minecraftforge.client.event.ColorHandlerEvent;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.common.ForgeModContainer;
-import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**

398592f7ef3e5e8c25fc95a45d5f3efbc9eaf732
TheRPGAdventurer <kingjamesxj9@gmail.com>
2019-06-15 08:34:32 +0800

    egs can be turned to blocks now

--- src/main/java/com/TheRPGAdventurer/ROTD/objects/entity/entitytameabledragon/interact/DragonInteract.java
+++ src/main/java/com/TheRPGAdventurer/ROTD/objects/entity/entitytameabledragon/interact/DragonInteract.java
@@ -10,15 +10,18 @@
 package com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.interact;
 
 import com.TheRPGAdventurer.ROTD.client.gui.GuiHandler;
+import com.TheRPGAdventurer.ROTD.objects.blocks.BlockDragonBreedEgg;
 import com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.EntityTameableDragon;
 import com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.breeds.DragonBreed;
 import com.TheRPGAdventurer.ROTD.util.DMUtils;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextComponentTranslation;
 
@@ -35,6 +38,14 @@ public class DragonInteract extends DragonInteractBase {
     public boolean interact(EntityPlayer player, ItemStack item) {
         if (dragon.isServer()) {
             if (isAllowed(player)) {
+                /*
+                 * Turning it to block
+                 */
+                if (dragon.isEgg() && player.isSneaking()) {
+                    dragon.world.playSound(player, dragon.getPosition(), SoundEvents.ENTITY_ZOMBIE_VILLAGER_CONVERTED, SoundCategory.PLAYERS, 1, 1);
+                    dragon.world.setBlockState(dragon.getPosition(), BlockDragonBreedEgg.DRAGON_BREED_EGG.getStateFromMeta(dragon.getBreedType().getMeta()));
+                    dragon.setDead();
+                }
 
                 /*
                  * Riding
@@ -50,8 +61,8 @@ public class DragonInteract extends DragonInteractBase {
                  */
                 if (player.isSneaking() && dragon.isTamedFor(player) && !hasInteractItemsEquipped(player)) {
                     // Dragon Inventory
-                	dragon.openGUI(player, GuiHandler.GUI_DRAGON);
-                	return true;
+                    dragon.openGUI(player, GuiHandler.GUI_DRAGON);
+                    return true;
                 }
             }
 
@@ -71,7 +82,7 @@ public class DragonInteract extends DragonInteractBase {
                 if (DMUtils.consumeFish(player) || DMUtils.consumeEquippedArray(player, DragonBreed.getFoodItems())) {
                     // Taming
                     if (!dragon.isTamed()) {
-                        dragon.tamedFor(player, dragon.getRNG().nextInt(15)==0);
+                        dragon.tamedFor(player, dragon.getRNG().nextInt(15) == 0);
                         eatEvent(player);
                     }
                     //  hunger
@@ -89,16 +100,16 @@ public class DragonInteract extends DragonInteractBase {
                 }
 
                 // Stop Growth
-                ItemFood shrinking=(ItemFood) DMUtils.consumeEquipped(player, dragon.getBreed().getShrinkingFood());
-                if (shrinking!=null) {
+                ItemFood shrinking = (ItemFood) DMUtils.consumeEquipped(player, dragon.getBreed().getShrinkingFood());
+                if (shrinking != null) {
                     dragon.setGrowthPaused(true);
                     eatEvent(player);
                     player.sendStatusMessage(new TextComponentTranslation("dragon.growth.paused"), true);
                     return true;
                 }
                 // Continue growth
-                ItemFood growing=(ItemFood) DMUtils.consumeEquipped(player, dragon.getBreed().getGrowingFood());
-                if (growing!=null) {
+                ItemFood growing = (ItemFood) DMUtils.consumeEquipped(player, dragon.getBreed().getGrowingFood());
+                if (growing != null) {
                     dragon.setGrowthPaused(false);
                     eatEvent(player);
                     return true;
@@ -114,14 +125,14 @@ public class DragonInteract extends DragonInteractBase {
     }
 
     private void spawnItemCrackParticles(Item item) {
-        for (int i=0; i < 15; i++) {
-            double motionX=dragon.getRNG().nextGaussian() * 0.07D;
-            double motionY=dragon.getRNG().nextGaussian() * 0.07D;
-            double motionZ=dragon.getRNG().nextGaussian() * 0.07D;
-            Vec3d pos=dragon.getAnimator().getThroatPosition();
-            double hx=pos.x;
-            double hy=pos.y;
-            double hz=pos.z;
+        for (int i = 0; i < 15; i++) {
+            double motionX = dragon.getRNG().nextGaussian() * 0.07D;
+            double motionY = dragon.getRNG().nextGaussian() * 0.07D;
+            double motionZ = dragon.getRNG().nextGaussian() * 0.07D;
+            Vec3d pos = dragon.getAnimator().getThroatPosition();
+            double hx = pos.x;
+            double hy = pos.y;
+            double hz = pos.z;
             // Spawn calculated particles
             dragon.world.spawnParticle(EnumParticleTypes.ITEM_CRACK, hx, hy, hz, motionX, motionY, motionZ, new int[]{Item.getIdFromItem(item)});
         }

e9609b99ac09ea3d51d31ecaa7c09c7951268218
TheRPGAdventurer <kingjamesxj9@gmail.com>
2019-06-18 17:50:07 +0800

    push matrix in banner is removed

--- src/main/java/com/TheRPGAdventurer/ROTD/client/render/dragon/layer/LayerRendererDragonBanner.java
+++ src/main/java/com/TheRPGAdventurer/ROTD/client/render/dragon/layer/LayerRendererDragonBanner.java
@@ -16,11 +16,11 @@ import net.minecraft.util.ResourceLocation;
 
 public class LayerRendererDragonBanner extends LayerRendererDragon {
 
-    private final ModelBanner bannerModel=new ModelBanner();
-    private final TileEntityBanner banner1=new TileEntityBanner();
-    private final TileEntityBanner banner2=new TileEntityBanner();
-    private final TileEntityBanner banner3=new TileEntityBanner();
-    private final TileEntityBanner banner4=new TileEntityBanner();
+    private final ModelBanner bannerModel = new ModelBanner();
+    private final TileEntityBanner banner1 = new TileEntityBanner();
+    private final TileEntityBanner banner2 = new TileEntityBanner();
+    private final TileEntityBanner banner3 = new TileEntityBanner();
+    private final TileEntityBanner banner4 = new TileEntityBanner();
 
     public LayerRendererDragonBanner(DragonRenderer renderer, DefaultDragonBreedRenderer breedRenderer, DragonModel model) {
         super(renderer, breedRenderer, model);
@@ -29,19 +29,18 @@ public class LayerRendererDragonBanner extends LayerRendererDragon {
 
     @Override
     public void doRenderLayer(EntityTameableDragon dragon, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-        ItemStack itemstack1=dragon.getBanner1();
-        ItemStack itemstack2=dragon.getBanner2();
-        ItemStack itemstack3=dragon.getBanner3();
-        ItemStack itemstack4=dragon.getBanner4();
+        ItemStack itemstack1 = dragon.getBanner1();
+        ItemStack itemstack2 = dragon.getBanner2();
+        ItemStack itemstack3 = dragon.getBanner3();
+        ItemStack itemstack4 = dragon.getBanner4();
 
         Minecraft mc = Minecraft.getMinecraft();
 
-        GlStateManager.pushMatrix();
 
-        if (itemstack1!=null && itemstack1.getItem()==Items.BANNER && banner1!=null) {
+        if (itemstack1 != null && itemstack1.getItem() == Items.BANNER && banner1 != null) {
             banner1.setItemValues(itemstack1, false);
             model.body.postRender(0.0625F);
-            ResourceLocation resourcelocation1=this.getBannerResourceLocation(banner1);
+            ResourceLocation resourcelocation1 = this.getBannerResourceLocation(banner1);
 //            GlStateManager.translate(0.7F, 0.4F, -0.6F);
             //lower x++ higher x--
             GlStateManager.translate(0.7F, 0.0, Interpolation.smoothStep(-2.6F, -0.6F, dragon.getAnimator().getSpeed())); // all of it is get speed or one was pitch?
@@ -53,20 +52,16 @@ public class LayerRendererDragonBanner extends LayerRendererDragon {
             GlStateManager.scale(0.625F, -0.625F, -0.625F);
             GlStateManager.color(1.0F, 1.0F, 1.0F, 1);
             renderer.bindTexture(resourcelocation1);
-            bannerModel.bannerStand.showModel=false;
+            bannerModel.bannerStand.showModel = false;
             bannerModel.renderBanner();
 
 
         }
 
-        GlStateManager.popMatrix();
-
-        GlStateManager.pushMatrix();
-
-        if (itemstack2!=null && itemstack2.getItem()==Items.BANNER && banner2!=null) {
+        if (itemstack2 != null && itemstack2.getItem() == Items.BANNER && banner2 != null) {
             banner2.setItemValues(itemstack2, false);
             model.body.postRender(0.0625F);
-            ResourceLocation resourcelocation2=this.getBannerResourceLocation(banner2);
+            ResourceLocation resourcelocation2 = this.getBannerResourceLocation(banner2);
 //            GlStateManager.translate(-0.7F, 0.4, -0.6F);
             //lower x++ higher x--
             GlStateManager.translate(-0.7F, 0.0, Interpolation.smoothStep(-2.6F, -0.6F, dragon.getAnimator().getSpeed()));
@@ -78,19 +73,15 @@ public class LayerRendererDragonBanner extends LayerRendererDragon {
             GlStateManager.scale(0.625F, -0.625F, -0.625F);
             GlStateManager.color(1.0F, 1.0F, 1.0F, 1);
             renderer.bindTexture(resourcelocation2);
-            bannerModel.bannerStand.showModel=false;
+            bannerModel.bannerStand.showModel = false;
             bannerModel.renderBanner();
 
         }
 
-        GlStateManager.popMatrix();
-
-        GlStateManager.pushMatrix();
-
-        if (itemstack3!=null && itemstack3.getItem()==Items.BANNER && banner3!=null) {
+        if (itemstack3 != null && itemstack3.getItem() == Items.BANNER && banner3 != null) {
             banner3.setItemValues(itemstack3, false);
             model.body.postRender(0.0625F);
-            ResourceLocation resourcelocation3=this.getBannerResourceLocation(banner3);
+            ResourceLocation resourcelocation3 = this.getBannerResourceLocation(banner3);
             GlStateManager.translate(-0.4F, -1.7F, 1.7F);
             GlStateManager.translate(0, Interpolation.smoothStep(2.9F, dragon.getAnimator().getModelOffsetY() + 1.7F, dragon.getAnimator().getSpeed()), 0);
             GlStateManager.translate(0, 0, Interpolation.smoothStep(-2.3F, dragon.getAnimator().getModelOffsetZ() + 1.0F, dragon.getAnimator().getSpeed()));
@@ -100,18 +91,14 @@ public class LayerRendererDragonBanner extends LayerRendererDragon {
             GlStateManager.scale(0.525F, -0.625F, -0.625F);
             GlStateManager.color(1.0F, 1.0F, 1.0F, 1);
             renderer.bindTexture(resourcelocation3);
-            bannerModel.bannerStand.showModel=false;
+            bannerModel.bannerStand.showModel = false;
             bannerModel.renderBanner();
         }
 
-        GlStateManager.popMatrix();
-
-        GlStateManager.pushMatrix();
-
-        if (itemstack4!=null && itemstack4.getItem()==Items.BANNER && banner4!=null) {
+        if (itemstack4 != null && itemstack4.getItem() == Items.BANNER && banner4 != null) {
             banner4.setItemValues(itemstack4, false);
             model.body.postRender(0.0625F);
-            ResourceLocation resourcelocation4=this.getBannerResourceLocation(banner4);
+            ResourceLocation resourcelocation4 = this.getBannerResourceLocation(banner4);
             GlStateManager.translate(0.4F, -1.7F, 1.7F);
             GlStateManager.translate(0, Interpolation.smoothStep(2.9F, dragon.getAnimator().getModelOffsetY() + 1.7F, dragon.getAnimator().getSpeed()), 0);
             GlStateManager.translate(0, 0, Interpolation.smoothStep(-2.3F, dragon.getAnimator().getModelOffsetZ() + 1.0F, dragon.getAnimator().getSpeed()));
@@ -121,12 +108,10 @@ public class LayerRendererDragonBanner extends LayerRendererDragon {
             GlStateManager.scale(0.525F, -0.625F, -0.625F);
             GlStateManager.color(1.0F, 1.0F, 1.0F, 1);
             renderer.bindTexture(resourcelocation4);
-            bannerModel.bannerStand.showModel=false;
+            bannerModel.bannerStand.showModel = false;
             bannerModel.renderBanner();
 
         }
-
-        GlStateManager.popMatrix();
     }
 
     private ResourceLocation getBannerResourceLocation(TileEntityBanner bannerObj) {

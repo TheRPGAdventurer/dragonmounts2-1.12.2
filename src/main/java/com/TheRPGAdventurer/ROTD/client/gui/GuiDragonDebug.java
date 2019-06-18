674d4361476b2713d4f7a91a8ea64fd95085e5ad
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-06-18 10:14:49 -0400

    Merge remote-tracking branch 'upstream/master'

--- src/main/java/com/TheRPGAdventurer/ROTD/client/gui/GuiDragonDebug.java
+++ src/main/java/com/TheRPGAdventurer/ROTD/client/gui/GuiDragonDebug.java
@@ -12,6 +12,7 @@ package com.TheRPGAdventurer.ROTD.client.gui;
 import com.TheRPGAdventurer.ROTD.DragonMounts;
 import com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.EntityTameableDragon;
 import com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.breeds.EnumDragonBreed;
+import com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.helper.DragonBreedHelper;
 import com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.helper.DragonLifeStageHelper;
 import com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.helper.DragonReproductionHelper;
 import net.minecraft.client.Minecraft;
@@ -304,7 +305,7 @@ public class GuiDragonDebug extends Gui {
      text.println("Breed points");
      text.setColor(WHITE);
         
-      com.TheRPGAdventurer.ROTD.server.entity.helper.DragonBreedHelper breedHelper = dragonServer.getBreedHelper();
+      DragonBreedHelper breedHelper = dragonServer.getBreedHelper();
       breedHelper.getBreedPoints().forEach((breedType, points) -> {
         text.setColor(breedType.getBreed().getColor());
         text.printf("%s: %d\n", breedType, points.get());
--- src/main/java/com/TheRPGAdventurer/ROTD/objects/entity/entitytameabledragon/helper/DragonBreedHelper.java
+++ src/main/java/com/TheRPGAdventurer/ROTD/objects/entity/entitytameabledragon/helper/DragonBreedHelper.java
@@ -7,7 +7,7 @@
  **    May you find forgiveness for yourself and forgive others.
  **    May you share freely, never taking more than you give.
  */
-package com.TheRPGAdventurer.ROTD.server.entity.helper;
+package com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.helper;
 
 import com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.EntityTameableDragon;
 import com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.breeds.DragonBreed;

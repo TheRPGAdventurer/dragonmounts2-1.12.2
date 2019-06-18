3f15bcc71822d396cee265a6f3a1c77b885d04c2
TheRPGAdventurer <kingjamesxj9@gmail.com>
2019-06-18 17:50:20 +0800

    added pushattrib

--- src/main/java/com/TheRPGAdventurer/ROTD/client/render/dragon/layer/LayerRendererDragonSaddle.java
+++ src/main/java/com/TheRPGAdventurer/ROTD/client/render/dragon/layer/LayerRendererDragonSaddle.java
@@ -4,24 +4,29 @@ import com.TheRPGAdventurer.ROTD.client.model.dragon.DragonModel;
 import com.TheRPGAdventurer.ROTD.client.render.dragon.DragonRenderer;
 import com.TheRPGAdventurer.ROTD.client.render.dragon.breeds.DefaultDragonBreedRenderer;
 import com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.EntityTameableDragon;
+import net.minecraft.client.renderer.GlStateManager;
 
 /**
  * Created by EveryoneElse on 14/06/2015.
  */
 public class LayerRendererDragonSaddle extends LayerRendererDragon {
-    
+
     public LayerRendererDragonSaddle(DragonRenderer renderer,
-            DefaultDragonBreedRenderer breedRenderer, DragonModel model) {
+                                     DefaultDragonBreedRenderer breedRenderer, DragonModel model) {
         super(renderer, breedRenderer, model);
     }
-    
+
     @Override
     public void doRenderLayer(EntityTameableDragon dragon, float moveTime,
-            float moveSpeed, float partialTicks, float ticksExisted, float lookYaw,
-            float lookPitch, float scale) {
-        if (!dragon.isSaddled()) {return;}
+                              float moveSpeed, float partialTicks, float ticksExisted, float lookYaw,
+                              float lookPitch, float scale) {
+        GlStateManager.pushAttrib();
+        if (!dragon.isSaddled()) {
+            return;
+        }
         renderer.bindTexture(breedRenderer.getSaddleTexture());
         model.render(dragon, moveTime, moveSpeed, ticksExisted, lookYaw, lookPitch, scale);
+        GlStateManager.popAttrib();
     }
 
     @Override

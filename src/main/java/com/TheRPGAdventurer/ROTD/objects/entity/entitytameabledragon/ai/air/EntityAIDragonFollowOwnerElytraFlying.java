5f1558ff79d13254a59752cc29df8ae945aceab5
TheRPGAdventurer <kingjamesxj9@gmail.com>
2019-06-18 17:50:42 +0800

    elytra fixes

--- src/main/java/com/TheRPGAdventurer/ROTD/objects/entity/entitytameabledragon/ai/air/EntityAIDragonFollowOwnerElytraFlying.java
+++ src/main/java/com/TheRPGAdventurer/ROTD/objects/entity/entitytameabledragon/ai/air/EntityAIDragonFollowOwnerElytraFlying.java
@@ -1,25 +1,23 @@
 /*
-** 2016 April 26
-**
-** The author disclaims copyright to this source code. In place of
-** a legal notice, here is a blessing:
-**    May you do good and not evil.
-**    May you find forgiveness for yourself and forgive others.
-**    May you share freely, never taking more than you give.
+ ** 2016 April 26
+ **
+ ** The author disclaims copyright to this source code. In place of
+ ** a legal notice, here is a blessing:
+ **    May you do good and not evil.
+ **    May you find forgiveness for yourself and forgive others.
+ **    May you share freely, never taking more than you give.
  */
 package com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.ai.air;
 
 import com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.EntityTameableDragon;
 import com.TheRPGAdventurer.ROTD.objects.entity.entitytameabledragon.ai.EntityAIDragonBase;
-
 import net.minecraft.entity.player.EntityPlayer;
 
 /**
- *
  * @author Nico Bergemann <barracuda415 at yahoo.de>
  */
 public class EntityAIDragonFollowOwnerElytraFlying extends EntityAIDragonBase {
-    
+
     protected EntityPlayer owner = (EntityPlayer) dragon.getOwner();
 
     public EntityAIDragonFollowOwnerElytraFlying(EntityTameableDragon dragon) {
@@ -28,34 +26,34 @@ public class EntityAIDragonFollowOwnerElytraFlying extends EntityAIDragonBase {
 
     @Override
     public boolean shouldExecute() {
-    	
-    	if (!dragon.canFly()) {
-    		return false;
-    	}
-        
+
+        if (!dragon.canFly()) {
+            return false;
+        }
+
         // don't follow if sitting
         if (dragon.isSitting()) {
-        	return false;
+            return false;
         }
-        
-        if(dragon.getLeashed()) {
-           return false;
+
+        if (dragon.getLeashed()) {
+            return false;
         }
-        
+
         // don't follow if ownerless 
         if (owner == null) {
             return false;
         }
-        
+
         // don't follow if already being ridden
         if (dragon.isPassenger(owner)) {
             return false;
         }
-        
+
         // follow only if the owner is using an Elytra
         return owner.isElytraFlying();
     }
-    
+
     @Override
     public void updateTask() {
         // liffoff
@@ -64,7 +62,7 @@ public class EntityAIDragonFollowOwnerElytraFlying extends EntityAIDragonBase {
         }
 
         // mount owner if close enough, otherwise move to owner
-        if (dragon.getDistance(owner) <= dragon.width * dragon.getScale() || dragon.getDistance(owner) <= dragon.height * dragon.getScale()) {
+        if (dragon.getDistance(owner) <= dragon.width || dragon.getDistance(owner) <= dragon.height && owner.isSneaking()) {
             owner.startRiding(dragon);
         }
 
